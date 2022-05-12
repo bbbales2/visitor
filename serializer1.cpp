4af1bdb45b21b2c6b4c3626ed1b3a58ab3386cec
Ben <bbbales2@gmail.com>
2022-05-12 07:16:45 -0700

    Swapped example order

--- serializer1.cpp
+++ serializer1.cpp
@@ -6,11 +6,6 @@ public:
   void visit(const B* b) override {
     std::cout << "B: " << b->val << std::endl;
   }
-
-  void visit(const D* d) override {
-    std::cout << "D: " << d->val << std::endl;
-    d->bval.accept(*this);
-  }
 };
 
 int main() {
@@ -26,6 +21,6 @@ int main() {
   std::cout << "Serializer1 does not know how to print c" << std::endl;
   static_cast<A*>(&c)->accept(serializer1);
 
-  std::cout << "Serializer1 knows how to print d and b" << std::endl;
+  std::cout << "Serializer1 does not know how to print d" << std::endl;
   static_cast<A*>(&d)->accept(serializer1);
 }
--- serializer2.cpp
+++ serializer2.cpp
@@ -6,6 +6,11 @@ public:
   void visit(const B* b) override {
     std::cout << "B: " << b->val << std::endl;
   }
+
+  void visit(const D* d) override {
+    std::cout << "D: " << d->val << std::endl;
+    d->bval.accept(*this);
+  }
 };
 
 int main() {
@@ -21,6 +26,6 @@ int main() {
   std::cout << "Serializer2 does not know how to print c" << std::endl;
   static_cast<A*>(&c)->accept(serializer2);
 
-  std::cout << "Serializer2 does not know how to print d" << std::endl;
+  std::cout << "Serializer2 knows how to print d and b" << std::endl;
   static_cast<A*>(&d)->accept(serializer2);
 }
